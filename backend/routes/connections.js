e62f204364c9ba4c55a1e588b20c8923cae8c56d
Alex Luangxay <alexluangxay@gmail.com>
2025-10-06 00:00:41 -0400

    Merge branch 'backend' into iteration-1

--- backend/routes/connections.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { searchConnections } = require('../controllers/connectionsController');
-
-router.get('/', searchConnections);
-
-module.exports = router;
